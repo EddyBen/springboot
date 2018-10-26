aad4707496c329af0ff386fdb0d05b2e08130a77
EddyBen <35094661+EddyBen@users.noreply.github.com>
2018-10-26 11:50:26 +0800

    Initial commit

---
+++ HelloController.java
@@ -0,0 +1,19 @@
+package com.example.springboot;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/hello")
+    public String hello(){
+        return "First SpringBoot";
+    }
+    @RequestMapping("/bye")
+    public String bye(){
+        return "First SpringBoot";
+    }
+    
+
+}
