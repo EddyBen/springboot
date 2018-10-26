fceca26d0ec2f0663ed7218c215d92dea0144a84
EddyBen <35094661+EddyBen@users.noreply.github.com>
2018-10-26 15:18:23 +0800

    10

--- Test1.java
+++ Test1.java
@@ -1,4 +1,9 @@
 package com.example.springboot;
 
 public class Test1 {
+    @Override
+    public int hashCode() {
+        System.out.println("Test1");
+        return super.hashCode();
+    }
 }
--- Test2.java
+++ Test2.java
@@ -1,4 +1,10 @@
 package com.example.springboot;
 
 public class Test2 {
+    @Override
+    public int hashCode() {
+        System.out.println("Test2");
+        return super.hashCode();
+
+    }
 }
